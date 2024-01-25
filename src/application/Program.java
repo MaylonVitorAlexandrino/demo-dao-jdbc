40f12c029eb4e920877683b53eead3180bee7c25
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-01-25 13:21:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 		
+		Seller seller = new Seller(1, "Alex Pink", "pink@gmail.com" , new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
