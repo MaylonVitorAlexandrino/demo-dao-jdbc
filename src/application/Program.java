ec79d7ab32e46fe826a3ec529a7d38e1e9bbf756
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-01-25 15:36:51 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,10 +28,16 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
-		System.out.println("\n===== TEST 4: seller findAll =====");
+		System.out.println("\n===== TEST 4: seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n===== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
 	}
 
 }
