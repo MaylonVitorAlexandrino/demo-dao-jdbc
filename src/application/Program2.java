6c4a6ff72f340f70c9ba426be30f35bda4fab7b2
Maylon Vitor Alexandrino <maylonvitor110283@gmail.com>
2024-01-26 12:53:52 -0300

    finished implementing department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,11 +1,13 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program2 {
 
@@ -22,5 +24,22 @@ public class Program2 {
 		System.out.println("\n===== TEST 2: department findAll =====");
 		List<Department> list = departmentDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n===== TEST 4: department insert =====");
+		dep = new Department(null, "Music");
+		departmentDao.insert(dep);
+		System.out.println("Inserted! New id = " + dep.getId());
+		
+		System.out.println("\n===== TEST 5: department update =====");
+		dep = departmentDao.findById(1);
+		dep.setName("Food");
+		departmentDao.update(dep);
+		System.out.println("Update complete!");
+		
+		System.out.println("\n===== TEST 6: department delete =====");
+		System.out.print("Enter the ID to be deleted: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
 	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,23 +20,74 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 	public DepartmentDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
-
 	
-	public void insert(Department obj) {
-		// TODO Auto-generated method stub
-		
+	@Override
+	public void insert(Department dep) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO department " +
+					"(Name) " +
+					"VALUES " +
+					"(?)", 
+					Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, dep.getName());
+			
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					dep.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
-	public void update(Department obj) {
-		// TODO Auto-generated method stub
-		
+	public void update(Department dep) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department " +
+					"SET Name = ? " +
+					"WHERE Id = ?");
+
+			st.setString(1, dep.getName());
+			st.setInt(2, dep.getId());
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -49,7 +101,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			rs = st.executeQuery();
 			if (rs.next()) {
 				Department dep = new Department(rs.getInt("Id"), rs.getString("Name"));
-				
+
 				return dep;
 			}
 			return null;
@@ -73,7 +125,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			List<Department> list = new ArrayList<>();
 
 			while (rs.next()) {
-				Department dep = new Department(rs.getInt("Id"),rs.getString("Name"));
+				Department dep = new Department(rs.getInt("Id"), rs.getString("Name"));
 				list.add(dep);
 			}
 			return list;
